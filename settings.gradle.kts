f3576b1410b3cdbfd613add07bb5de771a338d5b
András Oravecz <info@oandras.hu>
2026-09-24 15:15:15 +0200

    updated agp to 9.4.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.library") version "9.4.0"
+        id("com.android.library") version "9.4.1"
         id("org.jetbrains.kotlin.android") version "2.4.10"
         id("org.jetbrains.dokka") version "2.2.0"
         id("org.jetbrains.dokka-javadoc") version "2.2.0"
